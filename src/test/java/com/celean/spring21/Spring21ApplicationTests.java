ffe7d18b5194bbefa0df7ca0c62d761d511ff24f
celean77777 <celean77777@gmail.com>
2022-08-17 21:43:29 +0300

    Initial commit

---
+++ src/test/java/com/celean/spring21/Spring21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celean.spring21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
